cf4172d0defc223d3304958de8cb502eaa0a452c
Bragiattovsk <luisbragiatto4@gmail.com>
2025-02-10 16:53:26 -0300

    Adicionado lógica com javascript

---
+++ Pratica/formulario_contatos/main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form-contato');
+const nomes = [];
+const numeros = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha(){
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+
+    if (nomes.includes(inputNomeContato.value)){
+        alert(`O contato ${inputNomeContato.value} já foi inserido`);
+    } else{
+        nomes.push(inputNomeContato.value);
+        numeros.push(inputTelefoneContato.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputTelefoneContato.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputNomeContato = '';
+    inputTelefoneContato = '';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
